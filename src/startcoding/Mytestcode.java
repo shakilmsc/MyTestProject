6deaf9c2b243756cb82d1e89ac94b22e4f3c75d4
Shakil Ahmed <shakilmsc@gmail.com>
2018-01-17 14:44:23 -0500

    added code

---
+++ src/startcoding/Mytestcode.java
@@ -0,0 +1,11 @@
+package startcoding;
+
+public class Mytestcode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is just statrting writing code");
+		System.out.println("More I understand more I liked testing commiting and pusing ");
+	}
+
+}
